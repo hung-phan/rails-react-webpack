fc678923c3df4be7c4587e9f2673545b3d6096d5
hung-phan <colorvisavn@gmail.com>
2015-02-26 02:00:47 +0700

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,34 @@
 'use strict';
 
-var _       = require('lodash'),
-    argv    = require('minimist')(process.argv.slice(2)),
-    DEBUG   = (argv.env || "development") === "development",
-    webpack = require('webpack');
+var _     = require('lodash'),
+  argv    = require('minimist')(process.argv.slice(2)),
+  DEBUG   = (argv.env || "development") === "development",
+  webpack = require('webpack');
 
-var config = {
+var defaultConfig = {
+  cache: DEBUG,
+  debug: DEBUG,
+  devtool: DEBUG ? '#inline-source-map' : false,
+  resolve: {
+    modulesDirectories: ['node_modules', 'vendor/assets/bower_components'],
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [{
+      test: /.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader?experimental&optional=runtime'
+    }]
+  }
+};
+
+var config = _.extend({
   entry: {
     main: './app/assets/sources/main'
   },
   output: {
     path: './app/assets/javascripts/build',
-    publicPath: '/assets/',
+    publicPath: '/assets/build/',
     filename: '[name].bundle.js',
     chunkFilename: '[id].bundle.js'
   },
@@ -22,34 +39,15 @@ var config = {
     'react-router': 'window.ReactRouter',
     'superagent': 'window.superagent'
   },
-  resolve: {
-    modulesDirectories: ['node_modules', 'vendor/assets/bower_components'],
-    extensions: ['', '.js']
-  },
-  cache: DEBUG,
-  debug: DEBUG,
-  devtool: DEBUG ? '#inline-source-map' : false,
-  stats: {
-    colors: true,
-    reasons: DEBUG
-  },
   plugins: [
+    new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': DEBUG ? '"development"' : '"production"',
       '__DEV__': DEBUG
     }),
     new webpack.optimize.CommonsChunkPlugin('common.js')
-  ],
-  module: {
-    loaders: [
-      {
-        test: /.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader?experimental&optional=runtime'
-      }
-    ]
-  }
-};
+  ]
+}, defaultConfig);
 
 module.exports = config;
