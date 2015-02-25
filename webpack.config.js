96a03c44a81abab689910cb6d4ea57deb98f93ba
hung-phan <colorvisavn@gmail.com>
2015-02-25 15:48:47 +0700

    Update package.json

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var webpack = require('webpack'),
+    _       = require('lodash'),
+    argv    = require('minimist')(process.argv.slice(2));
+
+var DEBUG_MODE = !argv.production;
+
+/*
+ * Webpack configuaration
+ */
+
+var config = {
+  cache: DEBUG_MODE,
+  debug: DEBUG_MODE,
+  devtool: DEBUG_MODE ? '#inline-source-map' : false,
+  stats: {
+    colors: true,
+    reasons: DEBUG_MODE
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin()
+  ],
+};
+
+module.exports = config;
